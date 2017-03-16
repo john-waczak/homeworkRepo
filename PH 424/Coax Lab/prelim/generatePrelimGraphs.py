7c2279958162b511bcad155492fbef73800227e7
John_Waczak <waczakj@oregonstate.edu>
2017-03-16 08:58:08 -0700

    new folder and coax lab report

--- PH 424/Coax Lab/prelim/generatePrelimGraphs.py
+++ PH 424/Coax Lab/prelim/generatePrelimGraphs.py
@@ -2,9 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 R_top = 37.9
+
+
+
+
+L = 150                     #[m]
+R_0 =  R_top/L            #[ohms/m]
+C_0 = 93.5085*10**(-12)    #[F/m]
+v = 2.00*10**8           #[m/s]
+L_0 = 1/(v**2*C_0)  #[H/m]
+
+B = (R_0*C_0*v)/2
+print B
 r  = np.linspace(0, 400, 1000)
 R_id = (r-R_top)/(r+R_top)
 T_id =(2*r)/(r+R_top)
+
+Rv = R_id*np.e**(-2*B*L)
+Tv = T_id*np.e**(-B*L)
+
+
 
 dir = "C:/Users/John/Desktop/homeworkRepo/PH 424/Coax Lab/prelim"
 resistances = np.loadtxt(dir+"/resistances.txt")
@@ -13,14 +30,16 @@ vtrans_vinc = np.loadtxt(dir+"/vtrans_vinc.txt")
 
 plt.figure()
 plt.plot(resistances, vref_vinc, 'o', color = 'k', label="v_ref/v_inc")
-plt.plot(r, R_id, '--', color = 'k', label = "R_id, Rtop = 37.9 $\Omega$")
+plt.plot(r, R_id, '--', color = 'k', label = "R_id")
+plt.plot(r, Rv, color = 'k', label = "Rv")
 plt.plot(r, np.zeros(1000),  color= 'k')
 plt.plot(resistances, vtrans_vinc, 's', color = 'b',  label="v_trans/v_inc")
-plt.plot(r, T_id, '--', color = 'b', label = "T_id, Rtop = 37.9 $\Omega$")
+plt.plot(r, T_id, '--', color = 'b', label = "T_id")
+plt.plot(r, Tv, color = 'b', label = "Tv")
 plt.xlabel("Terminal Resistance [$\Omega$]")
 plt.ylabel("v/v_inc")
 plt.xlim(0,402)
-plt.legend(loc = 'lowerright')
+plt.legend(loc = 'lower right')
 plt.grid(True)
 
 
--- Ph 315/termPaper/EarthBBS.py
+++ Ph 315/termPaper/EarthBBS.py
@@ -7,20 +7,27 @@ c = 2.998*10**8                 #[m/s]
 T = 287                               #[K]
 
 
-lam =np.linspace (0.001, 100, 10000)
+lam =np.linspace (0.001*10**(-6), 100*10**(-6), 10000)
 
 def blackBody(lam, T):
-    A = (2*np.pi*h*c**2)/((lam*10**-6)**5)
-    B = 1/(np.exp(h*c/((lam*10**-6)*k*T))-1)
+    A = (2*np.pi*h*c**2)/(lam**5)
+    B = 1/(np.exp(h*c/(lam*k*T))-1)
     return A*B
 
-print "CO2 value: %f" % blackBody(14.95, T)
-print "CH4value: %f" % blackBody(7.65, T)
+k = np.linspace(0.01, 3000, 10000)
+def blackBodyK(k, T):
+    A = (2*np.pi*h*c**2)/((2*np.pi/k)**5)
+    B = 1/(np.exp(h*c/((2*np.pi/k)*k*T))-1)
+    return A*B
 
-plt.plot(lam, blackBody(lam, T), )
-plt.xlabel("$\lambda$ [$\mu$m]")
-plt.ylabel("Spectral Intensity [W/$m^2$/$\mu m$]")
-plt.ylim(0, 2.6*10**7)
-plt.xticks(np.arange(0, 100, 5))
+
+#print "CO2 value: %f" % blackBody(14.95*10**-6, T)
+#print "CH4value: %f" % blackBody(7.65*10**-6, T)
+
+plt.figure()
+plt.plot(lam, blackBody(lam, T) )
 plt.grid(True)
+
+plt.figure()
+plt.plot(k, blackBodyK(k,T))
 plt.show()
