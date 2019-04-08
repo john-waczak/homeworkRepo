858a2be0a413cab88d4513a7b8bc6ff39ece8e88
john-waczak <john.louis.waczak@gmail.com>
2019-04-08 14:05:52 -0700

    new folders and homework

---
+++ cs_162/template/struct.cpp
@@ -0,0 +1,13 @@
+
+
+#include<iostream>
+#include<string>
+#include<fstream>
+using std::string;
+
+
+struct MyStruct{
+  int myInt;
+  float myFloat;
+  string myString;
+};
---
+++ cs_162/template/struct.h
@@ -0,0 +1,7 @@
+#ifndef struct_h
+#define struct_h
+
+struct MyStruct;
+
+
+#endif 
