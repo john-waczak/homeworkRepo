67b7a78a0d9017468cd9d7f85c16c5f2ea08c186
john-waczak <john.louis.waczak@gmail.com>
2019-05-08 07:57:07 -0700

    hw update

---
+++ ph_653/homework/hw5/auto/hw5.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "hw5"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "geometry"
+    "graphicx"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "ulem"
+    "enumitem"
+    "pdfpages"
+    "empheq"
+    "listings")
+   (TeX-add-symbols
+    '("threevec" 2)
+    '("bvec" 1)
+    '("unit" 1)
+    '("basis" 1)
+    '("expectation" 1)
+    '("matrixel" 3)
+    '("bracket" 2)
+    '("ket" 1)
+    '("bra" 1)
+    '("prob" 2)
+    "R"
+    "C"
+    "F"
+    "E"
+    "M"
+    "sphere")
+   (LaTeX-add-environments
+    "solution")
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "corollary"
+    "lemma"
+    "definition"
+    "Example"
+    "Remark"))
+ :latex)
+
