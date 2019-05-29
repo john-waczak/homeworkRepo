011641ce2035e749e27d0d057c583b984dfaf7cf
john-waczak <john.louis.waczak@gmail.com>
2019-05-29 08:32:30 -0700

    homework update

---
+++ mth_437/assignments/hw7/auto/hw7.el
@@ -0,0 +1,58 @@
+(TeX-add-style-hook
+ "hw7"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "geometry"
+    "graphicx"
+    "amsmath"
+    "amssymb"
+    "amsthm"
+    "ulem"
+    "enumitem"
+    "pdfpages"
+    "empheq"
+    "listings"
+    "hyperref")
+   (TeX-add-symbols
+    '("threevec" 2)
+    '("bvec" 1)
+    '("unit" 1)
+    '("basis" 1)
+    '("expectation" 1)
+    '("matrixel" 3)
+    '("bracket" 2)
+    '("ket" 1)
+    '("bra" 1)
+    "R"
+    "C"
+    "F"
+    "E"
+    "M"
+    "sphere"
+    "del")
+   (LaTeX-add-environments
+    "solution")
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "corollary"
+    "lemma"
+    "definition"
+    "Example"
+    "Remark"))
+ :latex)
+
